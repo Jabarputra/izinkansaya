8108c4d6c2e2f2995c338a6ad303902a46a59673
Jabarputra <160265487+Jabarputra@users.noreply.github.com>
2024-08-08 23:33:15 +0700

    Add files via upload

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ cd
 mkdir /usr/local/sbin
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-red() { echo -e "\\033[1;32m${*}\\033[0m"; }
+red() { echo -e "\\033[32;1m${*}\\033[0m"; }
 clear
 fun_bar() {
     CMD[0]="$1"
@@ -19,20 +19,20 @@ fun_bar() {
         touch $HOME/fim
     ) >/dev/null 2>&1 &
     tput civis
-    echo -ne "  \033[0;32m Please Wait Loading \033[1;97m- \033[1;33m["
+    echo -ne "  \033[1;33m Please Wait Loading \033[1;37m- \033[0;33m["
     while true; do
         for ((i = 0; i < 18; i++)); do
-            echo -ne "\033[1;32m# "
+            echo -ne "\033[0;32m# "
             sleep 0.1s
         done
         [[ -e $HOME/fim ]] && rm $HOME/fim && break
-        echo -e "\033[1;33m]"
+        echo -e "\033[0;33m]"
         sleep 1s
         tput cuu1
         tput dl1
-        echo -ne "  \033[0;32m Please Wait Loading \033[1;37m- \033[0;33m["
+        echo -ne "  \033[1;33m Please Wait Loading \033[1;37m- \033[0;33m["
     done
-    echo -e "\033[1;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
+    echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
     tput cnorm
 }
 res1() {
@@ -53,13 +53,13 @@ res1() {
 }
 netfilter-persistent
 clear
-echo -e "\033[0;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e "\e[1;97;101m       UPDATE SCRIPT      \e[0m"
-echo -e "\033[0;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\e[1;97;101m         UPDATE SCRIPT         \e[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-echo -e " \033[0;91m update script service\033[1;36m"
+echo -e " \033[95m update script service\033[1;37m"
 fun_bar 'res1'
-echo -e "\033[0;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
+echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-read -n 1 -s -r -p "Tekan [ Enter ] untuk kembali menus"
-menus
+read -n 1 -s -r -p "Tekan [ Enter ] untuk kembali menu"
+menu
